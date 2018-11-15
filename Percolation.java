c0908f4da592507da9c89ad86d2d3b04704d5a44
mongmongi <ldh793692@gmail.com>
2018-11-15 22:25:51 +0900

    Initial commit

---
+++ Percolation.java
@@ -0,0 +1,35 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+
+    int n = 0;
+
+    public Percolation(int n){
+        this.n = n;
+    }
+
+    public boolean percolates(){
+
+        return  false;
+    }
+
+    public void open(int i, int j){
+
+    }
+
+    public boolean isOpen(int row, int col){
+
+
+        return false;
+    }
+
+    public boolean isFull(int row, int col){
+
+        return false;
+    }
+
+}
